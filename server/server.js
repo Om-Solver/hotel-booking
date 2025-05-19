0fd3169629c4f90511868c72c5964da74bd86c38
Omprakash Gupta <gomprakashgupta66@gmail.com>
2025-05-19 14:04:07 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 //API to listen Clerk webhooks
-app.post("/api/clerk", clerkWebhooks);
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res)=> res.send("API is working"))
 
