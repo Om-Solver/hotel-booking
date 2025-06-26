d479a3dbf6d00040ac20be9f6740534eb90cd6c8
Omprakash Gupta <gomprakashgupta66@gmail.com>
2025-06-26 14:03:23 +0530

    stripe added

--- server/server.js
+++ server/server.js
@@ -9,12 +9,16 @@ import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
+import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 connectDB()
 connectCloudinary();
 
 const app = express()
 app.use(cors()) //Enable Cross-Origin resource sharing
+
+// API to listen to Stripe Webhooks
+app.post('/api/stripe', express.raw({ type: "application/json" }), stripeWebhooks);
 
 //Middleware
 app.use(express.json())
@@ -23,7 +27,7 @@ app.use(clerkMiddleware())
 //API to listen Clerk webhooks
 app.use("/api/clerk", clerkWebhooks);
 
-app.get('/', (req, res)=> res.send("API is working"))
+app.get('/', (req, res) => res.send("API is working"))
 app.use('/api/user', userRouter)
 app.use('/api/hotels', hotelRouter)
 app.use('/api/rooms', roomRouter)
@@ -31,4 +35,4 @@ app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
